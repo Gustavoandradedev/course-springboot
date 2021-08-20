9d19ad6bee391d344d4e20f1edd0812db39f1aa4
Gustavo Andrade <gustavoandrade15@hotmail.com>
2021-08-19 21:23:07 -0300

    Project created

---
+++ src/test/java/br/com/zupinnovation/coursespringboot/CoursespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupinnovation.coursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
