9952a2d83ea4f975e4c12c15a9ee466fbd648fde
Gustavo Andrade <gustavoandrade15@hotmail.com>
2021-08-22 22:36:48 -0300

    OrderStatus enum

--- src/main/java/br/com/zupinnovation/coursespringboot/config/TestConfig.java
+++ src/main/java/br/com/zupinnovation/coursespringboot/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.zupinnovation.coursespringboot.entities.Order;
 import br.com.zupinnovation.coursespringboot.entities.User;
+import br.com.zupinnovation.coursespringboot.entities.enums.OrderStatus;
 import br.com.zupinnovation.coursespringboot.repositories.OrderRepository;
 import br.com.zupinnovation.coursespringboot.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2021-08-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2021-08-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2021-08-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2021-08-20T19:53:07Z"), OrderStatus.CANCELED, u1);
+		Order o2 = new Order(null, Instant.parse("2021-08-21T03:42:10Z"), OrderStatus.PAID, u2);
+		Order o3 = new Order(null, Instant.parse("2021-08-22T15:21:22Z"), OrderStatus.DELIVERED, u2);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		OrderRepository.saveAll(Arrays.asList(o1, o2, o3));
